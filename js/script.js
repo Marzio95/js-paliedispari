2d0f35bac8fecbd3831b39cf86374bdc2e94bb4b
Marzio95 <marziodellarocca14@gmail.com>
2022-03-01 16:48:18 +0100

    snack pari e dispari senza funzioni

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 
 let userScelta = prompt('Scegli tra pari o dispari:');
-let userNumber = prompt('Scegli un numero tra 1 e 5:');
+console.log(userScelta);
+let userNumber = parseInt(prompt('Scegli un numero tra 1 e 5:'));
+console.log(userNumber);
 
-Math.floor(Math.random() * 5) + 1;
+
+let randomNumber = Math.floor(Math.random() * 5) + 1;
+console.log(randomNumber);
+
+
+let somma = userNumber + randomNumber;
+console.log(somma);
+
+if (somma % 2 == 0 && userScelta == 'pari'){
+    console.log('win')
+} else if (somma % 2 == 1 && userScelta == 'dispari') {
+    console.log('win');
+} else {
+    console.log('loose');
+}
+
+
+
+// if (userScelta == pari){
+   
+// } else {
+//     console.log(loose)
+// }
+
+// if (userScelta == dispari){
+//     console.log(win)
+// } else {
+//     console.log(loose)
+// }
