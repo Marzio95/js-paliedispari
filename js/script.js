f18f80f02819b0b93c4d83b95a9faa488fd6168c
Marzio95 <marziodellarocca14@gmail.com>
2022-03-01 16:50:10 +0100

    funzione numero casuale

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ console.log(userScelta);
 let userNumber = parseInt(prompt('Scegli un numero tra 1 e 5:'));
 console.log(userNumber);
 
+function numeroCasuale(){
+    let ranNum = Math.floor(Math.random() * 5) + 1;
+    return ranNum;
+}
+
 
 let randomNumber = Math.floor(Math.random() * 5) + 1;
 console.log(randomNumber);
